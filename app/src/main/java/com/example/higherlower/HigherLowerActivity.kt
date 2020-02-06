e831c6594c34ee783c77686ff35e06dc0cc39dfa
Léon Imanuel <leondino0309@gmail.com>
2020-02-06 15:52:46 +0100

    Perfected layout to be exactly like the example #auti

--- app/src/main/java/com/example/higherlower/HigherLowerActivity.kt
+++ app/src/main/java/com/example/higherlower/HigherLowerActivity.kt
@@ -2,11 +2,25 @@ package com.example.higherlower
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_higher_lower.*
 
 class HigherLowerActivity : AppCompatActivity() {
+
+    //Variables
+    private var currentThrow: Int = 1
+    private var lastThrow: Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_higher_lower)
+        initView()
+    }
+
+    private fun initView(){
+        updateUI()
+    }
+
+    private fun updateUI(){
+        lastThrowText.text = getString(R.string.last_throw_text, lastThrow)
     }
 }
